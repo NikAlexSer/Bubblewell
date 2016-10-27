43b8a41d5c946c64279bf904171ada832aa8cbce
Alexander Nikiforov <alexandernikser@gmail.com>
2016-10-27 19:13:37 +0500

    Some fixes and improvements

--- app.js
+++ app.js
@@ -1,37 +1,32 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var engines = require('consolidate');
-
-var app = express();
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    data = require('./routes/data'),
+    routes = require('./routes/index'),
+    logger = require('morgan'),
+    engines = require('consolidate'),
+    app = express();
 
 // view engine setup
 app.engine('haml', engines.haml);
+app.engine('hbs', engines.handlebars);
 app.set('views', path.join(__dirname, 'views'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
+// requests and response monitor
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+// styles compilation
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   //outputStyle: "compressed"
-  //indentedSyntax: true,
-  //sourceMap: true
+  indentedSyntax: true,
+  sourceMap: true
 }));
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
-app.use('/users', users);
+app.use('/', data);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,29 +35,5 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error.hbs', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error.hbs', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
+// export app in global variables
 module.exports = app;

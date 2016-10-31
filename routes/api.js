f9829c4dcabadb509baf92bd85fcb75220677e64
Alexander Nikiforov <alexandernikser@gmail.com>
2016-10-31 18:40:14 +0500

    Write api for all project

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,41 @@
 var express = require('express'),
-    router = express.Router(),
-    data = require('../data/offers.json');
+  offers = require('../data/offers.json'),
+  helpers = require('handlebars-helpers')(),
+  users = require('../data/users.json'),
+  router = express.Router();
 
-/* GET users.json. */
-router.get('/offers', function(req, res, next){
-    console.log(data);
-    res.send(data);
+/**
+ * Генерация начальной страницы
+ **/
+router.get('/', function(req, res, next){
+  res.render('index.haml');
+});
+
+/**
+ * Получение JSON офферов
+**/
+router.get('/api/offers', function(req, res, next){
+    console.log(offers);
+    res.send(offers);
+});
+
+/**
+ * Получение JSON юзеров
+ **/
+router.get('/api/users', function(req, res, next){
+  console.log(users);
+  res.send(users);
+});
+
+/**
+ * Получение рендера шаблона офферов в хтмл
+ **/
+router.get('/api/render', function(req, res, next){
+  console.log(users.users[1].firstName + ", Я твой отец!");
+  res.render('offer.hbs', {
+    offers: offers.offers,
+    users: users.users
+  });
 });
 
 module.exports = router;
